e0344b82adfe0d1720beac69106b4472a3376d10
Awe_Oluwabambi <oluwabambi.awe@upperlink.ng>
2024-08-09 19:08:38 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  textIsShown: boolean = true;
+  currentClasses: any = {}
+  ngOnInit() {
+    this.setCurrentClasses();
+  }
   title = 'angular-assignment-solution';
+  items = [
+    {id: 1, name: 'John', gender: 'male'},
+    {id: 2, name: 'Mary', gender: 'female'},
+    {id: 3, name: 'Peter', gender: 'male'},
+    {id: 4, name: 'Moses', gender: 'male'},
+    {id: 5, name: 'Rebecca', gender: 'female'},
+  ]
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      fem: true
+    }
+  }
+
+  toggleTextShow() {
+    this.textIsShown = !this.textIsShown;
+  }
 }
